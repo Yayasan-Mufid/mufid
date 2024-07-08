769dc1fb426d7c37291a7a67c4230b7b605b200a
asif <rimbaborne@gmail.com>
2024-07-08 10:08:23 +0800

    keterangan program di data

--- resources/views/dashboard/peserta/data.blade.php
+++ resources/views/dashboard/peserta/data.blade.php
@@ -24,6 +24,9 @@
                                     Status
                                 </th>
                                 <th scope="col" class="px-6 py-4 font-bold text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                                    Program
+                                </th>
+                                <th scope="col" class="px-6 py-4 font-bold text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                     Aksi
                                 </th>
                             </tr>
@@ -35,6 +38,9 @@
                                 </th>
                                 <td class="px-6 py-4">
                                     Sedang Dalam Pembelajaran
+                                </td>
+                                <td class="px-6 py-4">
+                                  MIA
                                 </td>
                                 <td class="px-6 py-4">
                                   <Link href="{{ route('kelas') }}" class="py-2.5 px-5 text-sm font-medium text-primary-700 hover:text-white bg-white border border-primary-700 rounded-lg hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
@@ -50,6 +56,9 @@
                                   <span class="text-green-500">
                                     Selesai
                                   </span>
+                                </td>
+                                <td class="px-6 py-4">
+                                  MIA
                                 </td>
                                 <td class="px-6 py-4">
                                   <a href="{{ route('sertifikat') }}"  class="py-2.5 px-5 text-sm font-medium text-primary-700 hover:text-white bg-white border border-primary-700 rounded-lg hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300">
