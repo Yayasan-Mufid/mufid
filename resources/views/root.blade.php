4e58915778502b670c0dc690306a5673e5cb839a
rimbaborne <rimbaborne@gmail.com>
2025-02-03 15:57:50 +0800

    layout utama

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href="{{ asset('aset/img/m-logo.png') }}">
+        <title>{{ config('app.name', 'Mufid') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
