a3983e3c037db25d48c3467d71bd12efb62a3692
rimbaborne <rimbaborne@gmail.com>
2025-02-12 03:56:34 +0800

    belum readi info

--- resources/views/pages/informasi.blade.php
+++ resources/views/pages/informasi.blade.php
@@ -2,7 +2,7 @@
 
   <section>
     <div class="py-6">
-      <div class="max-w-7xl mx-auto px-10 sm:px-6 lg:px-8">
+      {{-- <div class="max-w-7xl mx-auto px-10 sm:px-6 lg:px-8">
         <div class="">
           <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             <!-- Blog Content Column -->
@@ -75,32 +75,8 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> --}}
     </div>
   </section>
 
-  <div class="py-6">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <footer class="bg-white rounded-lg shadow dark:bg-gray-800">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{ date('Y') }} <a href="#" class="hover:underline">Mufid</a>. All Rights Reserved.
-                </span>
-                <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-900 dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                    </li>
-                    <li>
-                        <a href="#" class="hover:underline">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    </div>
-</div>
 </x-app-layout>
