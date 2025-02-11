05516e093c5729ddd223a9d5e685866b3b66ca20
rimbaborne <rimbaborne@gmail.com>
2025-02-12 03:51:35 +0800

    title utama

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="shortcut icon" href="{{ asset('aset/img/m-logo.png') }}">
-        <title>{{ config('app.name', 'Mufid') }}</title>
+        <title>Yayasan Mufid</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
