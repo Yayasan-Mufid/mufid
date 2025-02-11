03d1091277596f764e45667717ffcd1ad4a3a9f2
rimbaborne <rimbaborne@gmail.com>
2025-02-12 03:47:58 +0800

    fix awal dashboard

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
-use App\Models\RoleToUnit;
 
 
 class DashboardController extends Controller
@@ -13,36 +12,41 @@ class DashboardController extends Controller
     public function index()
     {
         $roles = Role::all();
-        foreach (auth()->user()->roles as  $item) {
-            $roleuser[] = RoleToUnit::getByRole($item->name);
-        }
-        return view('dashboard', compact('roles', 'roleuser'));
+        $roleuser = auth()->user()->roles->map(fn ($role) => \App\Models\RoleToUnit::getByRole($role->name));
+        $data_peserta = \App\Models\Peserta::where(function($query) {
+                            $query->where('user_id', auth()->user()->id)
+                                ->orWhere('phone_number', auth()->user()->phone_number);
+                        })->get();
+        return view('dashboard', compact('roles', 'roleuser', 'data_peserta'));
     }
 
     public function akses()
     {
         $roles = Role::all();
-        foreach (auth()->user()->roles as  $item) {
-            $roleuser[] = RoleToUnit::getByRole($item->name);
-        }
+        $roleuser = auth()->user()->roles->map(fn ($role) => \App\Models\RoleToUnit::getByRole($role->name));
         return view('pages.akses', compact('roles', 'roleuser'));
     }
 
-    public function data()
-    {
-        $roles = Role::all();
-        foreach (auth()->user()->roles as  $item) {
-            $roleuser[] = RoleToUnit::getByRole($item->name);
-        }
-        return view('pages.data', compact('roles', 'roleuser'));
-    }
+    // public function data()
+    // {
+    //     $roles = Role::all();
+    //     $roleuser = auth()->user()->roles->map(fn ($role) => \App\Models\RoleToUnit::getByRole($role->name));
+    //     $data_peserta = \App\Models\Peserta::where('user_id', auth()->user()->id)->get();
+    //     return view('dashboard.peserta.data', compact('roles', 'roleuser', 'data_peserta'));
+    // }
+
+    // public function data_show($uuid)
+    // {
+    //     $roles = Role::all();
+    //     $roleuser = auth()->user()->roles->map(fn ($role) => \App\Models\RoleToUnit::getByRole($role->name));
+    //     $peserta = \App\Models\Peserta::where('uuid', $uuid)->first();
+    //     return view('dashboard.peserta.data', compact('roles', 'roleuser', 'peserta'));
+    // }
 
     public function pembayaran()
     {
         $roles = Role::all();
-        foreach (auth()->user()->roles as  $item) {
-            $roleuser[] = RoleToUnit::getByRole($item->name);
-        }
-        return view('pages.pembayaran', compact('roles', 'roleuser'));
+        $roleuser = auth()->user()->roles->map(fn ($role) => \App\Models\RoleToUnit::getByRole($role->name));
+        return view('dashboard.peserta.pembayaran', compact('roles', 'roleuser'));
     }
 }
