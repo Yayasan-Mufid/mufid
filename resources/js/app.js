52d15e18425cbc2da280da4e390362a9cdec420c
rimbaborne <rimbaborne@gmail.com>
2025-02-12 03:49:27 +0800

    js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,12 @@ import "@protonemedia/laravel-splade/dist/style.css";
 import 'flowbite';
 import "../css/app.css";
 import "./bootstrap";
-import CarouselSlider from "./components/CarouselSlider.vue";
 
 import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
 import { createApp } from "vue/dist/vue.esm-bundler.js";
+
+import BuktiTransferUpload from "./components/BuktiTransferUpload.vue";
+import CarouselSlider from "./components/CarouselSlider.vue";
 
 const el = document.getElementById("app");
 
@@ -16,8 +18,9 @@ createApp({
         "max_keep_alive": 10,
         "transform_anchors": false,
         "progress_bar": true,
-        "components": { 
-            CarouselSlider  
-        },  
+        "components": {
+            CarouselSlider,
+            BuktiTransferUpload
+        },
     })
     .mount(el);
